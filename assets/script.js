9a96cf550d9b9eb57a5d07440fda4147187b136c
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-06-19 18:49:00 -0500

    input city and search work

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,18 @@
 var apiKey = "298247b0e3697cf88be8ab01eb1fea69";
 //to store search keywords
 var keyCount = 0;
+//variable for current weather
 
-var url = "http://api.openweathermap.org/data/2.5/weather?q=London&Appid=" + apiKey;
 
-$.ajax({
-    url: url,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-
-});
+function search4City (){
+    var searchCity = $(".searchCity").val();
+    var urlCurrent = "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&Appid=" + apiKey;
+    console.log(searchCity);
+    $.ajax({
+        url: urlCurrent,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    
+    });
+}
