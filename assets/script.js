41165de235a56a1e8cf740228e9c5c632c030676
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-06-19 19:13:45 -0500

    created if statement when users input is blank

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,24 @@
 var apiKey = "298247b0e3697cf88be8ab01eb1fea69";
 //to store search keywords
 var keyCount = 0;
-//variable for current weather
 
 
 function search4City (){
     var searchCity = $(".searchCity").val();
+    //variable for current weather
     var urlCurrent = "http://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&Appid=" + apiKey;
-    console.log(searchCity);
-    $.ajax({
-        url: urlCurrent,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
     
-    });
+    if (searchCity == "") {
+        console.log("searchCity");
+    } else {    
+        $.ajax({
+            url: urlCurrent,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+        });
+    }
 }
+//append local date
+//5day forecast
+    //append to row
